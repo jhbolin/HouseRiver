ea4284c24131b6db7f9abf53e4b9e5ab9b84f24f
Joshua Bolin <jhbolin@yahoo.com>
2013-09-01 19:55:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseRiver::Application.config.secret_token = 'f3ba293dfba9307de6101b983c411662cf9753b991878419a4fa32097f92a51d4c5a3824aade45530d5ec3fc3f036f33b4fbdf255cd974c3571d1a8e2420e23f'
